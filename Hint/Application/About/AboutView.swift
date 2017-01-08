e8268b2145651c5a36e4664babcd7099ffa5964d
Chris Smith <csmith@uber.com>
2017-01-08 10:25:07 -0500

    clean up about window

---
+++ Hint/Application/About/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutController.swift
+//  Hint
+//
+//  Created by Christopher Smith on 1/7/17.
+//  Copyright © 2017 Chris Smith. All rights reserved.
+//
+
+import Cocoa
+
+class AboutView: NSView {
+    
+    @IBOutlet var sourceButton: NSButton!
+    
+    @IBAction func actionLinkClick(_ sender: NSButton) {
+        NSWorkspace.shared().open(Constants.sourceURL)
+    }
+}
+
+
--- Hint/Application/About/LinkButton.swift
+++ Hint/Application/About/LinkButton.swift
@@ -1,40 +1,31 @@
 //
-//  AboutController.swift
+//  LinkButton.swift
 //  Hint
 //
-//  Created by Christopher Smith on 1/7/17.
+//  Created by Christopher Smith on 1/8/17.
 //  Copyright © 2017 Chris Smith. All rights reserved.
 //
 
+import Foundation
 import Cocoa
-
-class AboutViewController: NSViewController {
-    
-    @IBOutlet var sourceButton: NSButton!
-    
-    @IBAction func actionLinkClick(_ sender: NSButton) {
-        NSWorkspace.shared().open(Constants.sourceURL)
-    }
-}
 
 class LinkButton: NSButton {
     
     override func viewWillDraw() {
         super.viewWillDraw()
-        self.attributedTitle = linkFromString(self.title, url: Constants.sourceURL)
+        self.attributedTitle = buildAttributedTitle(self.title)
     }
-        
+    
     override func resetCursorRects() {
         addCursorRect(bounds, cursor: NSCursor.pointingHand())
     }
     
-    func linkFromString(_ string: String, url: URL) -> NSAttributedString {
+    func buildAttributedTitle(_ string: String) -> NSAttributedString {
         
         let attr = NSMutableAttributedString(string: string)
         let range = NSMakeRange(0, attr.length)
         
         attr.beginEditing()
-        attr.addAttribute(NSLinkAttributeName, value: url.absoluteString, range: range)
         attr.addAttribute(NSForegroundColorAttributeName, value: NSColor.blue, range: range)
         attr.addAttribute(NSUnderlineStyleAttributeName, value: 1, range: range)
         attr.addAttribute(NSFontAttributeName, value: NSFont(name: "Helvetica Neue", size: 13)!, range: range)
