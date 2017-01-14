2b38ceae045523c942896e3d9b2cb50366bcb00c
Chris Smith <csmith@uber.com>
2017-01-14 15:40:47 -0800

    Revert "one test"
    This reverts commit 7afda0477319a94aca20540f1eae916e302a3170.

--- HintTests/HintTests.swift
+++ HintTests/HintTests.swift
@@ -11,27 +11,26 @@ import XCTest
 
 class HintTests: XCTestCase {
     
-    var scheduler: Scheduler!
-    
     override func setUp() {
         super.setUp()
-        
-        scheduler = Scheduler()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testSchedule() {
-        
-        let expect = expectation(description: "schedule")
-        scheduler.schedule(1, block: { () -> Void in
-            expect.fulfill()
-        })
-        
-        waitForExpectations(timeout: 5) { error in
-            XCTAssertNil(error)
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
         }
     }
+    
 }
