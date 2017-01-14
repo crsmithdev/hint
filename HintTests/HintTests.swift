ad6849df386764da0f845a8c08456ee3deb9f99d
Chris Smith <csmith@uber.com>
2017-01-14 15:41:42 -0800

    re-do test

--- HintTests/HintTests.swift
+++ HintTests/HintTests.swift
@@ -11,26 +11,27 @@ import XCTest
 
 class HintTests: XCTestCase {
     
+    var scheduler: Scheduler!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        scheduler = Scheduler()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSchedule() {
+        
+        let expect = expectation(description: "schedule")
+        scheduler.schedule(1, block: { () -> Void in
+            expect.fulfill()
+        })
+        
+        waitForExpectations(timeout: 5) { error in
+            XCTAssertNil(error)
         }
     }
-    
 }
